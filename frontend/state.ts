1d2928e1850ba60910da5de8dd322df9b05a3618
V. Bachinsky <bachinsky1@gmail.com>
2023-05-18 02:55:03 +0300

    Fix

--- frontend/state.ts
+++ frontend/state.ts
@@ -4,7 +4,7 @@ import { renderPage } from "./render"
 const setState = async (state: string) => {
     const url = window.location.origin + state
     history.pushState(null, '', url)
-    localStorage.setItem('state', url)
+    // localStorage.setItem('state', url)
     const data = await fetchData(window.location.origin + '/api' + state)
     renderPage(data)
 }
