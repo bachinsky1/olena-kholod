71d98b68481a367e2ce13346165da23fd0985339
V. Bachinsky <bachinsky1@gmail.com>
2023-05-18 11:38:57 +0300

    Autodeploy made

--- README.md
+++ README.md
@@ -3,14 +3,18 @@ Open some folder and:
 
 Run `git clone https://github.com/bachinsky1/olena-kholod.git .`
 
-Copy `.env.example` to `.env` and change DB connection data if it needs
+Run `bash run.sh` for authomaticaly deploying or make next steps
 
-Run `composer install`
+- Copy `.env.example` to `.env` and change DB connection data if it needs
 
-Run `npm install`
+- Run `composer install`
 
-Run `npx webpack`
+- Run `npm install`
 
-Run `cd public`
+- Run `npx webpack` if you want recompile typescript
 
-Run `php -S localhost:8000`
+- Run `cd public`
+
+- Run `php -S localhost:8000`
+
+- Open `http://localhost:8000`
--- run.sh
+++ run.sh
@@ -1,47 +1,65 @@
 
 #!/bin/bash
-echo "Now we are prepare base environment."
-echo "DB_HOST (default "localhost")": 
+clear
+
+echo "This script will automatically deploy the application to the local server."
+echo "Press any key to install all dependencies."
+read START
+clear
+
+composer install
+npm install
+clear
+
+echo "Dependencies installed."
+echo "Now create an empty database and press any key."
+read START
+clear
+
+echo "Please, set database connection environment."
+
+echo "DB_HOST (default "localhost"): "
 read DB_HOST
 
-if [[ $DB_HOST -eq "" ]]
+if [[ -z "$DB_HOST" ]]
 then 
     DB_HOST=localhost
 fi
 
-echo "DB_NAME (default "olena")": 
+echo "DB_NAME (default "olena"): " 
 read DB_NAME
 
-if [[ $DB_NAME -eq "" ]]
+if [[ -z "$DB_NAME" ]]
 then 
     DB_NAME=olena
 fi
 
-echo "DB_USER (default "root")": 
+echo "DB_USER (default "root") :" 
 read DB_USER
 
-if [[ $DB_USER -eq "" ]]
+if [[ -z "$DB_USER" ]]
 then 
     DB_USER=root
 fi
 
-echo "DB_PASS (default empty)": 
+echo "DB_PASS (default empty): " 
 read DB_PASS
 
-if [[ $DB_PASS -eq "" ]]
+if [[ -z "$DB_PASS" ]]
 then 
     DB_PASS=""
 fi
 
-echo "DB_PORT (default 3306)": 
+echo "DB_PORT (default 3306): " 
 read DB_PORT
 
-if [[ $DB_PORT -eq "" ]]
+if [[ -z "$DB_PORT" ]]
 then 
     DB_PORT=3306
 fi
 
-cat > .env << EOF
+
+cat <<EOF > .env
 DB_HOST=$DB_HOST
 DB_NAME=$DB_NAME
 DB_USER=$DB_USER
@@ -49,36 +67,16 @@ DB_PASS=$DB_PASS
 DB_PORT=$DB_PORT
 EOF
 
-php -r "\$conn = new mysqli('$DB_HOST', '$DB_USER', '$DB_PASS', '$DB_NAME', '$DB_PORT'); 
-    if (\$conn->connect_error) { 
-        die('Connection failed: ' . \$conn->connect_error); 
-    } 
-    echo 'Connected successfully';
-    \$sql = file_get_contents('mysql.sql');
-    \$res = \$conn->query(\$sql);
-    if (\$res) {
+echo "Trying connect to database and fill it..."
 
-        \$row = \$res->fetch_row();
-        echo \$row[0];
-    }
+php -f ./scripts/migration.php
 
-    \$res->close();
-    \$conn->close();
-    "
+echo ". Starting development server"
+cd public
+sleep 3
+clear
 
-echo "\n"
-echo It\'s nice to meet you $DB_HOST
-
-# new PDO("$type:host=$host;$port" . "dbname=$database;charset=$charset", $username, $password);
-# cp .env.example .env
-# # Установка зависимостей с помощью Composer
-# composer install
-
-# npm install
+start http://localhost:8000
+php -S localhost:8000
 
 
-# #npx webpack
-
-# cd public
-
-# php -S localhost:8000
