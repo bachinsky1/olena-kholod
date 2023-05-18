510b660a0611541e2a6e0703ee5034762b1ba0bf
V. Bachinsky <bachinsky1@gmail.com>
2023-05-18 11:50:30 +0300

    fix

--- scripts/migration.php
+++ scripts/migration.php
@@ -6,30 +6,15 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__ . "/..");
 $dotenv->load();
 
-$config = [
-    'username' => $_ENV['DB_USER'],
-    'database' => $_ENV['DB_NAME'],
-    'password' => $_ENV['DB_PASS'],
-    'type' => 'mysql',
-    'host' => $_ENV['DB_HOST'],
-    'port' => $_ENV['DB_PORT']
-];
-
 try {
     $dsn = "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']}";
     $conn = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS']);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    
-
     $sql = file_get_contents(__DIR__ .'/../mysql.sql');
-    // echo $sql;
-
+    
     $stmt = $conn->prepare($sql);
     $stmt->execute();
-
-    // $row = $stmt->fetch(PDO::FETCH_NUM);
-    // echo $row[0];
 
     $stmt->closeCursor();
     $conn = null;
